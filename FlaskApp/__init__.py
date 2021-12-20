e43e148872cd025819bac6ec962b44bfb287c758
D00233674 <73185717+D00233674@users.noreply.github.com>
2021-12-20 16:50:08 +0000

    updated FlaskApp
    Includes new encryption code as well as the code with model to access db

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, session, url_for, redirect, flash, jso
 from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 from functools import wraps
 
+from Cryptodome import Random
+from Cryptodome.Cipher import AES
+import secrets
+import base64
+import base64
+from hashlib import md5
+
 from flask_sqlalchemy import SQLAlchemy
 
 import json
@@ -9,18 +16,19 @@ import os
 import time, threading
 
 # we want access to the functionality defined in the my_db.py file
-#from . import my_db
+from . import my_db
 
 app = Flask(__name__)
 
 # set these up and test them - user - root ??
 # set them up in ssl wsgi environment file
+
 #mysql_user = os.getenv('mysql_user')
 #mysql_password = os.getenv('mysql_password')
 
-#app.config['SQLALCHEMY_DATABASE_URL']='mysql://'+mysql_user+':'+mysql_password+'@localhost/sd3biot22'
-#app.config['SQL_TRACK_MODIFICATIONS'] = False;
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URL']='mysql://root:Scooby6604!@localhost/ez_home'
+app.config['SQL_TRACK_MODIFICATIONS'] = False;
+db = SQLAlchemy(app)
 
 # add environment variables to ubuntu instance
 #facebook_app_id = os.getenv("sd3bFacebookApp")
@@ -31,6 +39,54 @@ app = Flask(__name__)
 
 alive = 0
 data = {}
+
+#add this------------------------------------------------------------------------------------------------------
+BLOCK_SIZE = 16
+
+def pad(data):
+    length = BLOCK_SIZE - (len(data) % BLOCK_SIZE)
+    return data + (chr(length) * length).encode()
+
+
+def unpad(data):
+    return data[:-(data[-1] if type(data[-1]) == int else ord(data[-1]))]
+
+
+def bytes_to_key(data, salt, output=48):
+    # extended from https://gist.github.com/gsakkis/4546068
+    assert len(salt) == 8, len(salt)
+    data += salt
+    key = md5(data).digest()
+    final_key = key
+    while len(final_key) < output:
+        key = md5(key + data).digest()
+        final_key += key
+    return final_key[:output]
+
+
+def encrypt(message, passphrase):
+    salt = Random.new().read(8)
+    key_iv = bytes_to_key(passphrase, salt, 32 + 16)
+    key = key_iv[:32]
+    iv = key_iv[32:]
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    return base64.b64encode(b"Salted__" + salt + aes.encrypt(pad(message)))
+
+
+def decrypt(encrypted, passphrase):
+    encrypted = base64.b64decode(encrypted)
+    assert encrypted[0:8] == b"Salted__"
+    salt = encrypted[8:16]
+    key_iv = bytes_to_key(passphrase, salt, 32 + 16)
+    key = key_iv[:32]
+    iv = key_iv[32:]
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(aes.decrypt(encrypted[16:]))
+
+
+password = "YourSecretKeyForEncryption&Descryption".encode()
+ct_b64 = "U2FsdGVkX1/7+GLNZ8n/JzMwCZ7tncsy6gpMYdCkXms="
+#---------------------------------------------------------------------------------------------------------------------------------
 
 
 @app.route('/')
@@ -117,10 +173,38 @@ def login():
         return "False"
 
 
+# # returns the house main room details
+# @app.route("/house_room", methods=['GET', 'POST'])
+# # returns the rooms that the house has
+# def home_rooms():
+#     rooms = [{
+#         "temperature": 10,
+#         "dateTime": "24:00:15T2021:12:02",
+#         "room": "Bedroom #1"
+#     },
+#         {
+#             "temperature": 20,
+#             "dateTime": "24:00:15T2021:12:02",
+#             "room": "Kitchen #2"
+#         }]
+#
+#     row = my_db.get_room_reading_row_if_exits(5)
+#     return row;
+#
+#     # if house_id == "1234567":
+#     #     return jsonify(row)
+#     #     #return str(row.room_reading_id) + ' | ' + str(row.temperature) + ' | ' + str(row.room_date_time)
+#     # else:
+#     #     return str(house_id)
+
 # returns the house main room details
 @app.route("/house_room/<house_id>", methods=['GET', 'POST'])
 # returns the rooms that the house has
 def home_rooms(house_id):
+    key = secrets.token_bytes(32)
+    print(type(key))
+    print(key)
+
     rooms = [{
         "temperature": 10,
         "dateTime": "24:00:15T2021:12:02",
@@ -132,7 +216,10 @@ def home_rooms(house_id):
             "room": "Kitchen #2"
         }]
     if house_id == "1234567":
-        return jsonify(rooms)
+        # rooms = jsonify(rooms);
+        rooms = str(rooms).encode()
+        rooms = encrypt(rooms,password)
+        return rooms
     else:
         return str(house_id)
 
@@ -145,11 +232,69 @@ def current_levels(house_id):
         "oil_level": 10,  # percent out a hundred in number
     }
     if house_id == "1234567":
-        return jsonify(result)
+        result = str(result).encode()
+        result = encrypt(result, password)
+        return result
     else:
         return str(house_id)
 
 #################################### New Endpoints End #########################################################
+
+# needed for home page
+@app.route("/average_temperature_house/<house_id>", methods=['GET'])
+def average_temperature(house_id):
+    # get the average temperature of the house
+    result = {
+        "temperature": 30,  # return as int
+    }
+    if house_id == "1234567":
+        result = str(result).encode()
+        result = encrypt(result, password)
+        return result
+    else:
+        return str(house_id)
+
+
+
+
+
+# get oil usage last 7 days
+@app.route("/usage/<house_id>", methods=['GET'])
+def usage_last_7(house_id):
+    # get the usage last 7 days
+    result = [{
+        "oilUsed": 100,  # oil used in litres
+        "day": "2021:11:02",
+    },
+        {
+            "oilUsed": 7000,  # oil used in litres
+            "day": "2021:11:03",
+        },
+        {
+            "oilUsed": 0,  # oil used in litres
+            "day": "2021:11:04",
+        },
+        {
+            "oilUsed": 50,  # oil used in litres
+            "day": "2021:11:05",
+        },
+        {
+            "oilUsed": 1000,  # oil used in litres
+            "day": "2021:11:06",
+        },
+        {
+            "oilUsed": 1000,  # oil used in litres
+            "day": "2021:11:07",
+        },
+        {
+            "oilUsed": 1000,  # oil used in litres
+            "day": "2021:11:086",
+        }]
+    if house_id == "1234567":
+        return result
+    else:
+        return str(house_id)
+
 
 if __name__ == '__main__':
     app.run()
--- FlaskApp/my_db.py
+++ FlaskApp/my_db.py
@@ -2,78 +2,73 @@ from flask_sqlalchemy import SQLAlchemy
 
 from .__init__ import db
 
-class user_table(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(4096))
-    user_id = db.Column(db.Integer)
-    authkey = db.Column(db.String(4096))
-    login = db.Column(db.Integer)
-    read_access = db.Column(db.Integer)
-    write_access = db.Column(db.Integer)
+class room_reading_table(db.Model):
+    __tablename__ = "room_reading"
+    room_reading_id = db.Column(db.Integer, primary_key = True)
+    room_id = db.Column(db.Integer)
+    temperature = db.Column(db.Float)
+    room_date_time = db.Column(db.DateTime)
 
-    def __init__(self, name, user_id, authkey, login, read_access, write_access):
-        self.name = name
-        self.user_id = user_id
-        self.authkey = authkey
-        self.login = login
-        self.read_access = read_access
-        self.write_access = write_access
+    def __init__(self, room_id, temperature, room_date_time):
+        self.room_id = room_id
+        self.temperature = temperature
+        self.room_date_time = room_date_time
 
 def delete_all():
     try:
-        db.session.query(user_table).delete()
+        db.session.query(room_reading_table).delete()
         db.session.commit()
-        print("Delete all done")
+        print("Delete all rooms done")
     except Exception as e:
         print("Failed " + str(e))
         db.session.rollback()
 
-def get_user_row_if_exits(user_id):
-    get_user_row = user_table.query.filter_by(user_id=user_id).first()
-    if get_user_row != None:
-        return get_user_row
+def get_room_reading_row_if_exits(room_reading_id):
+    get_room_reading_row = room_reading_table.query.filter_by(room_reading_id=room_reading_id).first()
+    if get_room_reading_row != None:
+        return get_room_reading_row
     else:
-        print("User doesn't exist")
+        print("Room reading doesn't exist")
         return False
 
-
-def add_user_and_login(name, user_id):
-    row = get_user_row_if_exits(user_id)
-    if(row != False):
-        row.login = 1
-        db.session.commit()
-    else:
-        print("Adding user " + name)
-        new_user = user_table(name, user_id, None, 1, 0, 0)
-        db.session.add(new_user)
-        db.session.commit()
-    print("user " + name + " login added")
-
-def user_logout(user_id):
-    row = get_user_row_if_exits(user_id)
-    if row != False:
-        row.login = 0
-        db.session.commit()
-        print("user " + row.name + " logged out")
-
-
-def add_auth_key(user_id, auth):
-    row = get_user_row_if_exits(user_id)
-    if row != False:
-        row.authkey = auth
-        db.session.commit()
-        print("user " + row.name + " authkey added")
-
-
-def view_all():
-    row = user_table.query.all()
-    for n in range(0, len(row)):
-        print(str(row[n].id) + ' | ' + row[n].name + ' | ' + str(row[n].user_id) + ' | ' + str(row[n].authkey) + ' | ' + str(row[n].login))
-
-
-def get_all_logged_in_users():
-    row = user_table.query.filter_by(login=1).all()
-    print("Logged in users: ")
-    for n in range(0, len(row)):
-        print(str(row[n].id) + ' | ' + row[n].name + ' | ' + str(row[n].user_id) + ' | ' + str(row[n].authkey) + ' | ' + str(row[n].login))
+#
+# def add_user_and_login(name, user_id):
+#     row = get_room_reading_row_if_exits(room_reading_id)
+#     if(row != False):
+#         row.login = 1
+#         db.session.commit()
+#     else:
+#         print("Adding user " + name)
+#         new_user = user_table(name, user_id, None, 1, 0, 0)
+#         db.session.add(new_user)
+#         db.session.commit()
+#     print("user " + name + " login added")
+#
+# def user_logout(user_id):
+#     row = get_user_row_if_exits(user_id)
+#     if row != False:
+#         row.login = 0
+#         db.session.commit()
+#         print("user " + row.name + " logged out")
+#
+#
+# def add_auth_key(user_id, auth):
+#     row = get_user_row_if_exits(user_id)
+#     if row != False:
+#         row.authkey = auth
+#         db.session.commit()
+#         print("user " + row.name + " authkey added")
+#
+#
+# def view_all():
+#     row = user_table.query.all()
+#     for n in range(0, len(row)):
+#         print(str(row[n].id) + ' | ' + row[n].name + ' | ' + str(row[n].user_id) + ' | ' + str(row[n].authkey) + ' | ' + str(row[n].login))
+#
+#
+# def get_all_logged_in_users():
+#     row = user_table.query.filter_by(login=1).all()
+#     print("Logged in users: ")
+#     for n in range(0, len(row)):
+#         print(str(row[n].id) + ' | ' + row[n].name + ' | ' + str(row[n].user_id) + ' | ' + str(row[n].authkey) + ' | ' + str(row[n].login))
+#
